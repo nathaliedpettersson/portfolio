2b47e7c1879a8319798acc28c3d20e482c43777b
nathaliedpettersson <nathalie.pettersson90@gmail.com>
2023-02-27 17:16:21 +0100

    add: simple info for visitors while portfolio is in progress

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>Portfolio in progress. Stay tuned for updates!</h1>
+      <p>Upon interest, please check out my GitHub while waiting...</p>
+      <div className="link-container">
+      <a href="https://github.com/nathaliedpettersson" target="_blank">REPOS</a>
+      </div>
     </div>
   );
 }
