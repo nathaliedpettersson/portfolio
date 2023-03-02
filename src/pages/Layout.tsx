e1106a11296bca5e7bd939dfb91ecfac3bb5aa38
nathaliedpettersson <nathalie.pettersson90@gmail.com>
2023-03-02 16:31:42 +0100

    add: import header to layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {Fragment} from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from '../components/Footer';
 
 // Import Footer and Header here
 // Call for this file in index.tsx and have it displayed on all pages on the website
 const Layout = () => {
-
+    return (
+<Fragment>
+<Header />
+<Outlet></Outlet>
+{/* <Footer /> */}
+</Fragment>
+    )
 };
 
 export default Layout;
