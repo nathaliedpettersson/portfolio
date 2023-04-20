9a3ff215226da3a4156bfc945603432a398343b7
nathaliedpettersson <nathalie.pettersson90@gmail.com>
2023-04-20 11:49:27 +0200

    refactor: small typecheck error

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Portfolio in progress. Stay tuned for updates!</h1>
-      <p>Take a look at my GitHub profile will waiting...</p>
+      <p>Take a look at my GitHub profile while waiting...</p>
       <div className="link-container">
       <a href="https://github.com/nathaliedpettersson" target="_blank">REPOS</a>
       </div>
