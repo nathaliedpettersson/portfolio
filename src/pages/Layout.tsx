fc48507800511584d6d2977c306842174a736573
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-07-17 14:11:10 +0200

    refactor: move files to components folder

--- src/pages/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// Import Footer and Header here
-// Call for this file in index.tsx and have it displayed on all pages on the website
-const Layout = () => {
-
-};
-
-export default Layout;
--- src/pages/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// Display this page under * in Routes and show it when URL can't be found
-// Show simple message and link back to Home page
-const NotFound = () => {
-
-};
-
-export default NotFound;
