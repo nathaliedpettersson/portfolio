9d07ded4eb6dbcb29d15acd1645cab5741d71db4
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-07-17 15:35:49 +0200

    feat: add tsparticles background

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { Link } from 'react-scroll';
 import './App.css';
+import { Container } from "react-dom";
 
 const App = () => {
 
@@ -9,16 +10,71 @@ const App = () => {
     await loadFull(main);
   }
 
-  const particlesLoaded = (container: any) => {
-    console.log(container);
-  };
-  
+  // const particlesLoaded = (container: any) => {
+  //   console.log(container);
+  // };
+
   return (
     <div className="App">
+         <Particles
+      id="tsparticles"
+      init={particlesInit}
+      // loaded={particlesLoaded}
+      options={{
+        background: {
+          color: "f1f1f1",
+        },
+        fpsLimit: 20,
+        particles: {
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: {
+              enable: true,
+              minimumValue: 0.5,
+            },
+            value: 1.4,
+          },
+          color: {
+            value: "rgb(0,28,28)", 
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1080,
+            },
+            limit: 0,
+            value: 800,
+          },
+          opacity: {
+            animation: {
+              enable: true,
+              minimumValue: 0.5,
+              speed: 1.2,
+              sync: false,
+            },
+            random: {
+              enable: true,
+              minimumValue: 0.1,
+            },
+            value: 1,
+          },
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              resize: true,
+            },
+          },
+        },
+      }} 
+      />
+      <div className="wrapper">
       <h1>Portfolio in progress. Stay tuned for updates!</h1>
       <p>Take a look at my GitHub profile while waiting...</p>
       <div className="link-container">
       <a href="https://github.com/nathaliedpettersson" target="_blank">REPOS</a>
+      </div>
       </div>
     </div>
   );
