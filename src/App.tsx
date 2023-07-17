76524bf77f46e03f7e982103b1476057ab284317
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-07-17 15:09:09 +0200

    feat: install tsparticles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import Particles from "react-tsparticles";
-import loadFull  from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import { Link } from 'react-scroll';
 import './App.css';
 
 const App = () => {
+
+  const particlesInit = async (main:any) => {
+    await loadFull(main);
+  }
+
+  const particlesLoaded = (container: any) => {
+    console.log(container);
+  };
+  
   return (
     <div className="App">
       <h1>Portfolio in progress. Stay tuned for updates!</h1>
