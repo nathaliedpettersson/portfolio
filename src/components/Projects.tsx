0e92514c3316ad224f93f27a2e9d7325c34a89aa
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-08-08 22:20:26 +0200

    refactor: styling of project placeholders

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,48 +3,58 @@ import './Projects.css';
 const Projects = () => {
     const placeholderImage = "https://i0.wp.com/plopdo.com/wp-content/uploads/2021/07/Screenshot-1.png?resize=1210%2C642&ssl=1";
 
-return (
-    <div className="project-container">
-        <div className="project-card">
-            <div className="project-card-image">
-                <img src={placeholderImage}></img>
-            </div>
-            <div className="project-card-content">
-            <span className="tag tag-teal">TYPESCRIPT</span>
-                {/* <span className="tag tag-black">CYPRESS</span> */}
-                <h4>Lorem ipsum project about lorem ipsum</h4>
-            </div>
-        </div>
-        <div className="project-card">
-            <div className="project-card-image">
-                <img src={placeholderImage}></img>
-            </div>
-            <div className="project-card-content">
-                <span className="tag tag-green">CSS</span>
-                <h4>Lorem ipsum project about lorem ipsum</h4>
-            </div>
+    return (
+        <div className="project-container">
+            <div className="project-card">
+                <div className="project-card-image">
+                    <img src={placeholderImage}></img>
+                </div>
+                <div className="project-card-tag">
+                    <span className="tag tag-teal">TYPESCRIPT</span>
+                    <span className="tag tag-black">CYPRESS</span>
+                    <span className="tag tag-orange">API</span>
+                </div>
+                <div className="project-card-content">
+                    <h4>Lorem ipsum project about lorem ipsum</h4>
+                </div>
             </div>
             <div className="project-card">
-            <div className="project-card-image">
-                <img src={placeholderImage}></img>
+                <div className="project-card-image">
+                    <img src={placeholderImage}></img>
+                </div>
+                <div className="project-card-tag">
+                    <span className="tag tag-purple">HTML</span>
+                    <span className="tag tag-green">CSS</span>
+                </div>
+                <div className="project-card-content">
+                    <h4>Lorem ipsum project about lorem ipsum</h4>
+                </div>
             </div>
-            <div className="project-card-content">
-                <span className="tag tag-coral">SVELTE</span>
-                <h4>Lorem ipsum project about lorem ipsum</h4>
+            <div className="project-card">
+                <div className="project-card-image">
+                    <img src={placeholderImage}></img>
+                </div>
+                <div className="project-card-tag">
+                    <span className="tag tag-coral">SVELTE</span>
+                </div>
+                <div className="project-card-content">
+                    <h4>Lorem ipsum project about lorem ipsum</h4>
+                </div>
             </div>
+            <div className="project-card">
+                <div className="project-card-image">
+                    <img src={placeholderImage}></img>
+                </div>
+                <div className="project-card-tag">
+                    <span className="tag tag-yellow">REACT</span>
+                </div>
+                <div className="project-card-content">
+                    <h4>Lorem ipsum project about lorem ipsum</h4>
+                </div>
+            </div>
+
         </div>
-        <div className="project-card">
-            <div className="project-card-image">
-                <img src={placeholderImage}></img>
-            </div>
-            <div className="project-card-content">
-                <span className="tag tag-yellow">REACT</span>
-                <h4>Lorem ipsum project about lorem ipsum</h4>
-            </div>
-        </div>
-       
-    </div>
-)
+    )
 };
 
 export default Projects;
