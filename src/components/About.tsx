f5af1aa8fcee9b7c71f3cf329c8d810d7413b71f
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-08-09 16:16:11 +0200

    refactor: add image and fix styling on about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,18 @@
 import './About.css';
+import ProfileImage from '../assets/images/IMG-3162.jpg';
 
 const About = () => {
     return (
         <div className="container">
-            <h1>hi, I am Nathalie.</h1>
-            <h3>Portfolio is under construction.
-                <br></br> Still curious?</h3>
-            <h3>Check out my GitHub.</h3>
+            <div className="about-content">
+                <h1>hello, i am Nathalie.</h1>
+                <h4 className="about-text">Portfolio is under construction.</h4>
+                <h4 className="about-text">Still curious?</h4>
+                <h4 className="about-text">Check out my GitHub.</h4>
+            </div>
+            <div className="about-image">
+                <img className="profile-image" src={ProfileImage} alt="Profile image" />
+            </div>
         </div>
     )
 }
