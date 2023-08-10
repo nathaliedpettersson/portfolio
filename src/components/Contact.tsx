db2f3ff9ff86a23886c5382eea43a41f8e124120
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-08-10 22:30:02 +0200

    wip: contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import './Contact.css';
+import SwedishResume from '../assets/images/CV-Nathalie-Pettersson.pdf';
+import EnglishResume from '../assets/images/CV-English-Nathalie-Pettersson.pdf';
 
-// Display contact info such as Email, Phone, GitHub and Linkedin (also displayed in footer)
 const Contact = () => {
-
+return (
+    <div className="contact-container">
+<div className="contact-info">
+<h2>Want to know more? Let's talk!</h2>
+    <div className="contact-card">
+<p>Email</p>
+    </div>
+    <div className="contact-card">
+<p>Linkedin</p>
+    </div>
+    <div className="contact-card">
+<p>Visit my GitHub</p>
+    </div>
+    <div className="contact-card">
+<a className="resume" href={SwedishResume} target="_blank">Swedish CV</a>
+<a className="resume" target="_blank" href={EnglishResume}>English resume</a>
+    </div>
+</div>
+    </div>
+)
 };
 
 export default Contact;
