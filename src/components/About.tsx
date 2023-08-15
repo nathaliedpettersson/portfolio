a3eeb3076a889e709a0fcbf6d9da921d2151675d
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-08-15 12:59:04 +0200

    bug: try to redeploy without react-typed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import ReactTyped from 'react-typed';
+// import ReactTyped from 'react-typed';
 import './About.css';
 import ProfileImage from '../assets/images/IMG-3162.jpg';
 
@@ -6,8 +6,8 @@ const About = () => {
     return (
         <div className="container">
             <div className="about-content">
-                <h1>
-                    <ReactTyped strings={["I'm Nathalie, a frontend developer."]} typeSpeed={60} loop={false} showCursor={false} />
+                <h1>I'm Nathalie, a frontend developer.
+                    {/* <ReactTyped strings={["I'm Nathalie, a frontend developer."]} typeSpeed={60} loop={false} showCursor={false} /> */}
                 </h1>
                 <h4 className="about-text">Portfolio is under construction.</h4>
                 <h4 className="about-text">Still curious?</h4>
