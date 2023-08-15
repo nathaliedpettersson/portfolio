f57f9bdf69578ed3325f5d9efb21824504867ac0
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-08-15 15:38:55 +0200

    refactor: remove react-typed from project

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,3 @@
-// import ReactTyped from 'react-typed';
 import './About.css';
 import ProfileImage from '../assets/images/IMG-3162.jpg';
 
@@ -7,7 +6,6 @@ const About = () => {
         <div className="container">
             <div className="about-content">
                 <h1>I'm Nathalie, a frontend developer.
-                    {/* <ReactTyped strings={["I'm Nathalie, a frontend developer."]} typeSpeed={60} loop={false} showCursor={false} /> */}
                 </h1>
                 <h4 className="about-text">Portfolio is under construction.</h4>
                 <h4 className="about-text">Still curious?</h4>
