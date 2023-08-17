e565aff7c1efd5337e99b04fda67da79c9b9bfa9
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-08-17 21:23:55 +0200

    refactor: change styling for contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,8 @@
 import './Contact.css';
-import EnglishResume from '../assets/images/CV-English-Nathalie-Pettersson.pdf';
+import EmailLogo from '../assets/images/logo-email.svg';
+import LinkedinLogo from '../assets/images/logo-linkedin.svg';
+import GithubLogo from '../assets/images/logo-github.svg';
+import ResumeLogo from '../assets/images/logo-resume.svg';
 import SwedishResume from '../assets/images/CV-Nathalie-Pettersson.pdf';
 
 const Contact = () => {
@@ -10,20 +13,21 @@ const Contact = () => {
     return (
         <div className="contact-container">
             <div className="contact-info">
-                <h2>Want to know more? Let's talk! 💬</h2>
+                <div className="divider"></div>
+                <h2>Want to know more? Let's talk!</h2>
                 <div className="contact-card">
-                    <p aria-label="Email">Mail: nathalie.pettersson90@gmail.com</p>
+                    <button onClick={() => window.location.href = 'mailto: nathalie.pettersson90@gmail.com'}><img src={EmailLogo} aria-label="Email" /></button>
                 </div>
                 <div className="contact-card">
-                    <a className="link" href={linkedinLink} target="_blank" aria-label="Linkedin"><p>Linkedin</p></a>
+                    <a className="link" href={linkedinLink} target="_blank" aria-label="Linkedin"><img src={LinkedinLogo} /></a>
                 </div>
                 <div className="contact-card">
-                    <a className="link" href={githubLink} target="_blank" aria-label="GitHuB"><p>Visit my GitHub</p></a>
+                    <a className="link" href={githubLink} target="_blank" aria-label="GitHuB"><img src={GithubLogo} /></a>
                 </div>
                 <div className="contact-card">
-                    <a className="resume" href={SwedishResume} target="_blank" aria-label="View swedish resume">Swedish CV</a>
-                    <a className="resume" href={EnglishResume} target="_blank" aria-label="View english resume">English resume</a>
+                    <a className="resume" href={SwedishResume} target="_blank" aria-label="View swedish resume"><img src={ResumeLogo} /></a>
                 </div>
+                <div className="divider"></div>
             </div>
         </div>
     )
