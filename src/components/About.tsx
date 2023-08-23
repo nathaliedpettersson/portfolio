7445fb9189a2224de04cdd3f20e092dd46fd3dff
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-08-23 16:56:59 +0200

    refactor: add new content in About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,11 +7,9 @@ const About = () => {
             <div className="about-content">
                 <h1>I'm Nathalie, a frontend web developer.
                 </h1>
-                {/* <h4 className="about-text">Portfolio is under construction.</h4>
-                <h4 className="about-text">Still curious?</h4>
-                <h4 className="about-text">Check out my GitHub down below by using the link.</h4> */}
-            <p className="about-text">My curiosity for tech comes from those typical scenes in a tv show or movie where they are typing fast on the keyboard, doing cool things or hacking into stuff. And yes, that's actually the truth.</p>
-            <p className="about-text">I decided to do something about that and pursue a career within IT by learning Frontend development through a school in Malmö, Sweden.</p>
+                <p className="about-text">My curiosity for tech comes from those typical scenes in a TV show or movie where they are typing fast on the keyboard, doing incredible things or hacking into stuff. And yes, that's actually the truth.</p>
+                <p className="about-text">I decided to do something about that and pursue a career within IT by learning Frontend development through a school in Malmö, Sweden.</p>
+                <p className="about-text">Key values for me in a workplace are diversity, communication and always striving to improve. The tech industry is always on the move, and therefore so should our enthusiasm and learning curve.</p>
             </div>
             <div className="about-image">
                 <img className="profile-image" src={ProfileImage} alt="Profile image" />
