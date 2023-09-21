b6a0f55b01eb0f3d568072825296eea445992630
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-09-21 21:55:02 +0200

    refactor: small changes in content for About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
                 </h1>
                 <p className="about-text">My curiosity for tech comes from those typical scenes in a TV show or movie where they are typing fast on the keyboard, doing incredible things or hacking into stuff. And yes, true story.</p>
                 <p className="about-text">Key values for me in a workplace are diversity, communication and always striving to improve. The tech industry is always on the move, and therefore so should our enthusiasm and learning curve.</p>
-                <p className="about-text">And hey, I encourage you to visit my GitHub to get a sense of what I'm currently working on  &#128421;  There are also more projects there, but I sampled a few down below.</p>
+                <p className="about-text">I sampled a few projects below but I do encourage you also to check out my GitHub for more and to see what I am currently working on.</p>
             </div>
             <div className="about-image">
                 <img className="profile-image" src={ProfileImage} alt="Profile image" />
