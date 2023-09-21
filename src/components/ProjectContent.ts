6d90ea5b1297ebfbd83af5868443d8f5e51bf24a
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-09-21 22:00:49 +0200

    feat: wip (content for projects props)

---
+++ src/components/ProjectContent.ts
@@ -0,0 +1,27 @@
+// This file is not in use yet
+
+import ChatAppImage from '../assets/images/chat-app.png';
+import VercelLogo from '../assets/images/logo-vercel.svg';
+import GithubLogo from '../assets/images/logo-github.svg';
+
+const ProjectContent = [
+    {   image:  ChatAppImage,
+        tag: ["TYPESCRIPT", "FIREBASE"],
+        title: "GRIT - Chat Application 💬 👋",
+        vercelLink: "https://nathaliedpettersson.github.io/Chat-App/",
+        vercelLogo: VercelLogo,
+        githubLink: "https://github.com/nathaliedpettersson/Chat-App",
+        githubLogo: GithubLogo
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+export default ProjectContent;
