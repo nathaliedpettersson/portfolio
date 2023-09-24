41dfe0028ff0232e00dcd685664755a9259a5e18
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-09-24 17:57:08 +0200

    feat: add all projects in array of ProjectContent

--- src/components/ProjectContent.ts
+++ src/components/ProjectContent.ts
@@ -1,25 +1,47 @@
 // This file is not in use yet
 
 import ChatAppImage from '../assets/images/chat-app.png';
+import ChuckNorrisImage from '../assets/images/chuck-norris.png';
+import DogApiImage from '../assets/images/dog-directory.png';
+import ReactCinemaImage from '../assets/images/react-cinema.png';
 import VercelLogo from '../assets/images/logo-vercel.svg';
 import GithubLogo from '../assets/images/logo-github.svg';
 
 const ProjectContent = [
     {   image:  ChatAppImage,
         tag: ["TYPESCRIPT", "FIREBASE"],
-        title: "GRIT - Chat Application 💬 👋",
+        projectTitle: "GRIT - Chat Application 💬 👋",
         vercelLink: "https://nathaliedpettersson.github.io/Chat-App/",
         vercelLogo: VercelLogo,
         githubLink: "https://github.com/nathaliedpettersson/Chat-App",
         githubLogo: GithubLogo
     },
+    {   image: ChuckNorrisImage,
+        tag: ["HTML", "CSS", "JAVASCRIPT", "API"],
+        projectTitle: "Chuck Norris Joke Generator",
+        vercelLink: "https://chuck-norris-joke-generator.vercel.app/",
+        VercelLogo: VercelLogo,
+        githubLink: "https://github.com/nathaliedpettersson/Chuck-Norris-Joke-Generator",
+        githubLogo: GithubLogo
+    },
     {
+        image: DogApiImage,
+        tag: ["JEST", "API", "REACT"],
+        projectTitle: "Dog Directory API with testing",
+        vercelLink: "https://dog-directory-api-with-jest.vercel.app/",
+        VercelLogo: VercelLogo,
+        githubLink: "https://github.com/nathaliedpettersson/DogDirectoryAPI-with-Jest",
+        githubLogo: GithubLogo
 
     },
     {
-
-    },
-    {
+        image: ReactCinemaImage,
+        tag: ["REACT", "JAVASCRIPT", "LOCAL STORAGE"],
+        projectTitle: "GRIT - My Movie Cinema App",
+        vercelLink: "https://react-cinema-blue.vercel.app/",
+        VercelLogo: VercelLogo,
+        githubLink: "https://github.com/nathaliedpettersson/Slutprojekt-JS3-ReactCinema",
+        githubLogo: GithubLogo
 
     }
 ]
