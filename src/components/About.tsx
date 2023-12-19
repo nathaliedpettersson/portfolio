10434f4d2f997a7e0cc2711d3f5303af2fe5146d
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-12-19 17:16:46 +0100

    refactor: few changes in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,9 @@ const About = () => {
             <div className="about-content">
                 <h1>I'm Nathalie, a frontend web developer.
                 </h1>
-                <p className="about-text">My curiosity for tech comes from those typical scenes in a TV show or movie where they are typing fast on the keyboard, doing incredible things or hacking into stuff. And yes, true story.</p>
-                <p className="about-text">Key values for me in a workplace are diversity, communication and always striving to improve. The tech industry is always on the move, and therefore so should our enthusiasm and learning curve.</p>
-                <p className="about-text">I sampled a few projects below but I do encourage you also to check out my GitHub for more and to see what I am currently working on.</p>
+                <p className="about-text">My curiosity for tech comes from those typical scenes in a TV show or movie where they are typing fast on the keyboard, doing incredible things or hacking into stuff. Yes, true story.</p>
+                <p className="about-text">I'd love to put my skills to work and be a part of a team and company where we all strive together to make the world a better place. You can also find me as a volunteer at Pink Programming which is an organization encouraging women and non-binary people to get into tech. </p>
+                <p className="about-text">I sampled a few projects below, but I do encourage you to check out my GitHub for more and to see what I am currently working on.</p>
             </div>
             <div className="about-image">
                 <img className="profile-image" src={ProfileImage} alt="Profile image" />
